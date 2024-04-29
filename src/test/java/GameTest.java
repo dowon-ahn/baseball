6a4050fa75d9d8d08ba80fc1c99c854ab09ea4dc
dowon-ahn <ahndw55@gmail.com>
2024-04-29 09:56:05 +0900

    .

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void testGame() {
+        Game game = new Game();
+    }
+}
