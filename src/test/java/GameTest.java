7b5890c1309d871da769ae484e5059cac547fba1
dowon-ahn <ahndw55@gmail.com>
2024-04-29 10:55:38 +0900

    null argument guard clause 추가

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,11 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
+    Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
 
     @Test
-    void testGame() {
+    void createGame() {
         Game game = new Game();
+        assertNotNull(game);
+    }
+    @Test
+    void argumentNullHandleTest() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            game.guess(null);
+        });
     }
 }
